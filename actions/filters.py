4e8488c75dbf2d3ff855365978269a2fcc8eb345
OndrejPech <pech.ondrej@gmail.com>
2022-08-31 15:51:49 +0200

    added default/initial value for show_passes filter

--- actions/filters.py
+++ actions/filters.py
@@ -4,9 +4,20 @@ from django.db.models import Q
 
 
 class ActionFilter(django_filters.FilterSet):
-    def __init__(self, *args, **kwargs):
+
+    def __init__(self, data=None, *args, **kwargs):
+
+        # if filterset is bound, use initial values as defaults
+        if data is not None:
+            data = data.copy()  # get a mutable copy of the QueryDict
+            for name, filter_object in self.base_filters.items():
+                initial = filter_object.extra.get('initial')
+                # filter param is either missing or empty, use initial as default
+                if not data.get(name) and initial:
+                    data[name] = initial
+
         self.team_id = kwargs.pop('team_id')  # new kwarg argument added
-        super().__init__(*args, **kwargs)
+        super().__init__(data, *args, **kwargs)
         # override fields
         self.filters['type'].label = 'typ'
         self.filters['game'].queryset = Game.objects.filter(Q(home_team_id=self.team_id) | Q(away_team_id=self.team_id))
@@ -16,11 +27,12 @@ class ActionFilter(django_filters.FilterSet):
         self.filters['passive_player'].queryset = Player.objects.filter(teams=self.team_id)
         self.filters['passive_player'].label = 'pas. hráč'
 
-    CHOICES = (
-        ('attack', 'útočí'), ('defence', 'brání')
-    )
+    TEAM_CHOICES = (('attack', 'útočí'), ('defence', 'brání'))  # (value, label)
+    PASS_CHOICES = (('no', 'ne'), ('yes', 'ano'))
 
-    team_in_possession = django_filters.ChoiceFilter(label='můj tým', choices=CHOICES, method='attack_or_defence')
+    team_in_possession = django_filters.ChoiceFilter(label='můj tým', choices=TEAM_CHOICES, method='attack_or_defence')
+    # The only one filter with initial value
+    show_passes = django_filters.ChoiceFilter(label='zobrazit nahrávky', choices=PASS_CHOICES, method='show_pass', initial='no')
 
     class Meta:
         model = Action
@@ -31,3 +43,9 @@ class ActionFilter(django_filters.FilterSet):
             return queryset.filter(team=self.team_id)
         elif value == 'defence':
             return queryset.filter(opp_team=self.team_id)
+
+    def show_pass(self, queryset, name,  value):
+        if value == 'no':
+            return queryset.exclude(type=2)  # ID 2 belongs to shoot # TODO read file to auto input this id
+        else:
+            return queryset
