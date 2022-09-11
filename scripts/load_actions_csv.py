4145400c79c4e1a2505c2024bdcacdb575cf3241
OndrejPech <pech.ondrej@gmail.com>
2022-09-11 20:46:26 +0200

    change script , based on CSV template

--- scripts/load_actions_csv.py
+++ scripts/load_actions_csv.py
@@ -3,7 +3,7 @@ from actions.models import Action
 
 # https://django-extensions.readthedocs.io/en/latest/runscript.html
 
-file_path = '/Users/ondrejpech/Desktop/game_01.csv'
+file_path = '/Users/ondrejpech/Desktop/backup matches/zruc-sla 1 half.csv'
 
 
 def run():
@@ -12,17 +12,19 @@ def run():
     :return:
     """
     with open(file_path) as file:
-        reader = csv.reader(file, delimiter=';')
+        reader = csv.reader(file, delimiter=',')
         next(reader)  # skip headers
 
         for row in reader:
-            # TODO update when final CSV template will be known
-            action, created = Action.objects.get_or_create(type_id=row[1],
-                                                           game_id=row[2],
-                                                           team_id=row[4],
+            action, created = Action.objects.get_or_create(type_id=row[4],
+                                                           game_id=row[3],
+                                                           team_id=row[5],
                                                            opp_team_id=row[6],
+                                                           left_pitch_team_id=row[7],
+                                                           right_pitch_team_id=row[8],
                                                            game_time=row[9],
                                                            video_time=row[10])
 
-            if created:
+            if not created:
                 print(f'{Action} exists')
+                print(row)
