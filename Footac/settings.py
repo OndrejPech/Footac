cd0f2e9aaf48ede85bbf88060cb8e82c0eada406
OndrejPech <pech.ondrej@gmail.com>
2022-09-28 13:28:11 +0200

    changed project language setup to czech

--- Footac/settings.py
+++ Footac/settings.py
@@ -112,7 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'cs'
+LANGUAGES = (
+    ('cs', u'Čeština'),)
 
 TIME_ZONE = 'Europe/Prague'
 
