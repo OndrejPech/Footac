77201ede32d10e047599771cde24a1e68acb94ab
OndrejPech <pech.ondrej@gmail.com>
2022-10-02 15:04:43 +0200

    changed method parameters name

--- actions/templatetags/custom_tags.py
+++ actions/templatetags/custom_tags.py
@@ -4,8 +4,8 @@ register = template.Library()
 
 
 @register.simple_tag
-def start_end_video(link, video_time, type):
-    """return videolink with start and end parameters base on type of action"""
+def start_end_video(link, video_time, ttype):
+    """return video-link with start and end parameters base on type of action"""
     # TODO can I use Integer as key for faster search or different way how to speed lookups?
     action_types = {'shot': (5, 20),
                     'pass': (2, 10),
@@ -24,11 +24,11 @@ def start_end_video(link, video_time, type):
                     }
 
     try:
-        start_end = action_types[type]
+        start_end = action_types[ttype]
         start = video_time - start_end[0]
         end = video_time + start_end[1]
     except KeyError:
-        print(f'{type} not found by start_end_video tag')
+        print(f'{ttype} not found by start_end_video tag')
         start = video_time - 5
         end = video_time + 10
 
@@ -47,7 +47,7 @@ def update_url(value, key, urlencode=None):
         querystring = urlencode.split('&')   # ['page=2','id=5',...]
         # remove the item with mentioned key
         filtered_querystring = filter(lambda p: p.split('=')[0] != field_name, querystring)
-        encoded_querystring = '&'.join(filtered_querystring)  #'id=5&type=4...'
+        encoded_querystring = '&'.join(filtered_querystring)  # 'id=5&type=4...'
         url = f'{url}&{encoded_querystring}'
 
     return url
