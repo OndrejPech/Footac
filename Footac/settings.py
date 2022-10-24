5e26168abc18188a6aea92734274af55f4e26476
OndrejPech <pech.ondrej@gmail.com>
2022-10-24 17:39:06 +0200

    secret key and allowed host changed for deployment

--- Footac/settings.py
+++ Footac/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5s6ia^kx!-#^nqag^=vk#^gi&ho$4#!^dvherwo8-iti8q4yvd'
+# SECRET_KEY = 'django-insecure-5s6ia^kx!-#^nqag^=vk#^gi&ho$4#!^dvherwo8-iti8q4yvd'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['footac.herokuapp.com']
 
 
 # Application definition
