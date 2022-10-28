1bf2935ee46a90528396d972d68145ef130fac78
OndrejPech <pech.ondrej@gmail.com>
2022-10-28 20:43:38 +0200

    install and use django_heroku

--- Footac/settings.py
+++ Footac/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +143,5 @@ LOGIN_REDIRECT_URL = 'actions:home'
 LOGOUT_REDIRECT_URL = 'accounts:login_user'
 
 LOGIN_URL = 'accounts:login_user'
+
+django_heroku.settings(locals())
