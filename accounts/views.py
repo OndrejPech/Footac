4a57a1f169794fc133c6593d2754e9323f1387e1
OndrejPech <pech.ondrej@gmail.com>
2023-01-12 12:09:19 +0100

    Remove import of reverse func. from django.shortcuts

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib import messages
 from .forms import SignUpForm, EditProfileForm, ChangePasswordForm
